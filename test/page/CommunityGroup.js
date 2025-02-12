29d6e471141e618f6e0c68953c24f28db31dbdd0
sarath153 <sarathuser25@gmail.com>
2025-02-12 11:58:27 +0530

    CommunityGroup

--- test/page/CommunityGroup.js
+++ test/page/CommunityGroup.js
@@ -1,5 +1,106 @@
 export class CommunityLocators{
 
+get eldercare(){
+    return $(`~button_Program.elderCare`)
+}
 
-    
+get logo(){
+    return $(`~app_logo_action`)
+}
+get Nurserecommendations(){
+    return $(`~Nurse recommendations`)
+}
+get MyResources(){
+    return $(`~My Resources`)
+}
+get CommunityGroups(){
+    return $(`~Community Groups`)
+}
+get CommunityEvents(){
+    return $(`~Community Events`)
+}
+get HealthAssessment(){
+    return $(`~Health Assessment`)
+}
+get comminityGroup(){
+    return $(`//android.widget.ImageView[@content-desc="Community Groups_action"]`)
+}
+get open(){
+    return $(`//android.view.View[@resource-id="open_community groups_text"]`)
+}
+get CommunityCountval(){
+    return $(`//android.view.View[@resource-id="5_text"]`)
+}
+get group(){
+    return $(`//android.view.View[@resource-id="test automation group_text"]`)
+}
+get GroupVal(){
+    return $(`//android.view.View[@content-desc="Test automation group"]`)
+}
+get GroupValPage(){
+    return $(`//android.view.View[@resource-id="test automation group_text"]`)
+}
+get posts(){
+    return $(`(//android.view.View[@content-desc="1 Posts"])[1]`)
+}
+get postesva(){
+    return $(`//android.view.View[@content-desc="Posts:"]`)
+}
+get automationPost(){
+    return $(`//android.view.View[@content-desc="automation post"]`)
+}
+get test(){
+    return $(`~tets`);
+}
+get newpost(){
+    return $(`//android.widget.Button[@content-desc="group_request_card_action"]/android.widget.ImageView`)
+}
+get automationdate(){
+    return $(`//android.view.View[@content-desc="February 11, 2025"]`)
+}
+get testoption(){
+    return $(`//android.widget.ImageView[@content-desc="post_card_action_67ab5de3d3579acc15db111c"]`)
+}
+get likepage(){
+    return $(`(//android.view.View[@resource-id="automation post_text"])[1]`)
+}
+get Browsearticlesabout(){
+    return $(`~Browse articles about:`)
+}
+get image(){
+    return $(`//android.widget.ImageView[@content-desc="image_view_action"]`)
+}
+get like(){
+    return $(`//android.widget.Button[@content-desc="like_button"]/android.widget.Button/android.view.View`)
+}
+get saveButton(){
+    return $(`//android.widget.Button[@content-desc="save_button"]/android.widget.Button/android.view.View`)
+}
+get commonds(){
+    return $(`//android.view.View[@resource-id="comments (0)_text"]`)
+}
+get clickonlike(){
+    return $(`//android.widget.Button[@content-desc="like_button"]/android.widget.Button/android.view.View/android.widget.ImageView`);
+}
+get afterLike(){
+    return $(`//android.view.View[@resource-id="(1)_text"]`)
+}
+get beforelike(){
+    return $(`//android.view.View[@resource-id="(0)_text"]`)
+}
+get addComment(){
+    return $(`//android.widget.ImageView[@content-desc="Add Comment"]`)
+}
+get clickonimage(){
+    return $(`//android.widget.ImageView[@content-desc="image_view_action"]`)
+}
+get imagescreen(){
+    return $(`//android.widget.ImageView`)
+}
+get imagesScreenClose(){
+    return $(`//android.widget.Button`)
+}
+
+
+
 }
--- test/specs/CommunityGroupElderCare.js
+++ test/specs/CommunityGroupElderCare.js
@@ -24,17 +24,7 @@ describe('Carepath Automation', async () => {
     let iterationValue = dataSets(iterations)
     let testName = '';
 
-    beforeEach(async function () {
-        try {
-            testName = this.currentTest.title;
-            console.log(`Starting test: ${testName}`);
-        } catch (error) {
-            console.error(`Error in beforeEach for test ${this.currentTest.title}:`, error.message);
-        }
-    });
-
-
-    it('Community Group Elder Care', async () => {
+    it('TS_001Community Group Elder Care', async () => {
         allureReporter.addDescription(`
             1. Click on the "Start Now" button.
             2. Check if the login screen is displayed.
@@ -42,17 +32,22 @@ describe('Carepath Automation', async () => {
             4. Check if the login button is enabled.
             5. Handle any dialog boxes related to notifications.
             6. Log in successfully if all steps are completed.
+            7. Click on Elder care
+            8.Verify that the user is able to see all the groups given inside the Community Groups tab just by clicking on the Open button.
 
         `);
         try {
 
-            
+            const mail = await readData1("Community", "ElderCare", "Mail", "Testdata1");
+            const Password = await readData1("Community", "ElderCare", "Password", "Testdata1");
+
             await Keywords.waitForDisplay(locator.startNow, 60000, "Start Now Button")
             await Keywords.click(locator.startNow, "Start Now Button")
             const loginDisplay = await Keywords.isDisplayed(locator.loginScreen, "Login screen")
             expect(loginDisplay).to.be.true;
-            await Keywords.SetValue(locator.userName, process.env.USER_NAME);
-            await Keywords.SetValue(locator.password, process.env.PASSWORD);
+
+            await Keywords.SetValue(locator.userName, mail);
+            await Keywords.SetValue(locator.password, Password);
             if (!await locator.loginButton.isDisplayed()) {
                 await browser.hideKeyboard();
             }
@@ -65,14 +60,203 @@ describe('Carepath Automation', async () => {
             await Keywords.click(locator.allowNotificationButton, "Allow notification button");
             console.log('Login process completed successfully.');
             await Keywords.waitForDisplay(locator.homePage, 45000, "Home Page")
-            await Keywords.verifyElementDisplayed(locator.mentalHealthCard, "Mental Health card")
-            await Keywords.click(locator.mentalHealthCard, "Mental Health card")
-            await Keywords.waitForDisplay(onboardLocator.mentalHealthText, 60000, "Mental Health Text");
+            await Keywords.verifyElementDisplayed(community.eldercare, "Elder care")
+            await Keywords.click(community.eldercare, "Elder card");
+            await driver.pause(5000);
+            await Keywords.verifyElementDisplayed(onboardLocator.pickADifferentProgram, "Pick a different program button");
+            await Keywords.verifyElementIsEnabled(onboardLocator.continueButton, "Continue Button");
+            await Keywords.click(onboardLocator.continueButton, "Continue Button");
+            await Keywords.waitForDisplay(community.logo, 45000, "logo");
+            await Keywords.verifyElementDisplayed(community.Nurserecommendations, "Nursere commendations");
+            await Keywords.verifyElementDisplayed(community.CommunityGroups, "Community Groups");
+            await Keywords.verifyElementDisplayed(community.CommunityEvents, "Community Events");
+            await Keywords.verifyElementDisplayed(community.MyResources, "My Resources");
+            await Keywords.verifyElementDisplayed(community.MyResources, "My Resources");
+            await Keywords.verifyElementDisplayed(community.open, "open");
+            await Keywords.click(community.open, "Open Button");
+            await driver.pause(5000);
+            await Keywords.verifyText(community.group, "content-desc", "Test automation group", "Test automation group");
+            await Keywords.verifyElementDisplayed(community.posts, "Posts");
+            await Keywords.click(community.GroupVal, "Test automation group");
+            await Keywords.verifyText(community.GroupValPage, "content-desc", "Test automation group", "Test automation group");
+            await driver.pause(9000);
+            await Keywords.verifyElementDisplayed(community.postesva, "post:");
+            await Keywords.verifyElementDisplayed(community.test, "test");           
+            await Keywords.verifyElementDisplayed(community.newpost, "new post");  
+            await Keywords.verifyElementDisplayed(community.automationdate, "Date");          
+
+
         } catch (err) {
             throw new Error(err);
         }
 
     });
+
+    it('TS_002Community Group Elder Care', async () => {
+        allureReporter.addDescription(`
+            1. Click on the "Start Now" button.
+            2. Check if the login screen is displayed.
+            3. Enter the username and password.
+            4. Check if the login button is enabled.
+            5. Handle any dialog boxes related to notifications.
+            6. Log in successfully if all steps are completed.
+            7. Click on Elder care
+            8.Verify that when the user likes the post whether the icon changes to a blue color and whether the count gets increased or not.
+
+        `);
+        try {
+
+            const mail = await readData1("Community", "ElderCare", "Mail", "Testdata1");
+            const Password = await readData1("Community", "ElderCare", "Password", "Testdata1");
+
+            await Keywords.waitForDisplay(locator.startNow, 60000, "Start Now Button")
+            await Keywords.click(locator.startNow, "Start Now Button")
+            const loginDisplay = await Keywords.isDisplayed(locator.loginScreen, "Login screen")
+            expect(loginDisplay).to.be.true;
+
+            await Keywords.SetValue(locator.userName, mail);
+            await Keywords.SetValue(locator.password, Password);
+            if (!await locator.loginButton.isDisplayed()) {
+                await browser.hideKeyboard();
+            }
+            const loginBtnEnable = await Keywords.isEnabled(locator.loginButton, "Login button")
+            expect(loginBtnEnable).to.be.true;
+            await Keywords.click(locator.loginButton, "Login Button")
+            await Keywords.waitForDisplay(locator.allowButton, 20000, "Get notified dialog box")
+            await Keywords.click(locator.allowButton, "Allow button");
+            await Keywords.waitForDisplay(locator.allowNotificationButton, 20000, "Allow notification")
+            await Keywords.click(locator.allowNotificationButton, "Allow notification button");
+            console.log('Login process completed successfully.');
+            await Keywords.waitForDisplay(locator.homePage, 45000, "Home Page")
+            await Keywords.verifyElementDisplayed(community.eldercare, "Elder care")
+            await Keywords.click(community.eldercare, "Elder card");
+            await driver.pause(5000);
+            await Keywords.verifyElementDisplayed(onboardLocator.pickADifferentProgram, "Pick a different program button");
+            await Keywords.verifyElementIsEnabled(onboardLocator.continueButton, "Continue Button");
+            await Keywords.click(onboardLocator.continueButton, "Continue Button");
+            await Keywords.waitForDisplay(community.logo, 45000, "logo");
+            await Keywords.verifyElementDisplayed(community.Nurserecommendations, "Nursere commendations");
+            await Keywords.verifyElementDisplayed(community.CommunityGroups, "Community Groups");
+            await Keywords.verifyElementDisplayed(community.CommunityEvents, "Community Events");
+            await Keywords.verifyElementDisplayed(community.MyResources, "My Resources");
+            await Keywords.verifyElementDisplayed(community.MyResources, "My Resources");
+            await Keywords.verifyElementDisplayed(community.open, "open");
+            await Keywords.click(community.open, "Open Button");
+            await driver.pause(5000);
+            await Keywords.verifyText(community.group, "content-desc", "Test automation group", "Test automation group");
+            await Keywords.verifyElementDisplayed(community.posts, "Posts");
+            await Keywords.click(community.GroupVal, "Test automation group");
+            await Keywords.verifyText(community.GroupValPage, "content-desc", "Test automation group", "Test automation group");
+            await driver.pause(9000);
+            await Keywords.verifyElementDisplayed(community.postesva, "post:");
+            await Keywords.verifyElementDisplayed(community.test, "test");           
+            await Keywords.verifyElementDisplayed(community.newpost, "new post");  
+            await Keywords.verifyElementDisplayed(community.automationdate, "Date");          
+            await Keywords.click(community.testoption, "test option");   
+            await driver.pause(9000);
+            await Keywords.verifyText(community.likepage, "content-desc", "automation post", "automation post");
+            await Keywords.verifyElementDisplayed(community.test, "test");
+            await Keywords.verifyElementDisplayed(community.Browsearticlesabout, "Browse articles about:");
+            await Keywords.verifyElementDisplayed(community.image, "image");    
+            await Keywords.verifyElementDisplayed(community.like, "like Button");   
+            await Keywords.verifyElementDisplayed(community.saveButton, "save Button");   
+            await Keywords.verifyText(community.commonds, "content-desc", "Comments (0)", "Comments (0)");
+            await Keywords.verifyText(community.beforelike, "content-desc", "(0)", "(0)");
+            await Keywords.click(community.clickonlike, "like"); 
+            await driver.pause(1000);
+            await Keywords.verifyText(community.afterLike, "content-desc", "(1)", "(1)");
+            await Keywords.click(community.clickonlike, "like"); 
+            await driver.pause(1000);
+            await Keywords.verifyText(community.beforelike, "content-desc", "(0)", "(0)");
+            await Keywords.verifyElementIsEnabled(community.addComment, "Add Comment Button");
+
+
+        } catch (err) {
+            throw new Error(err);
+        }
+
+    });
+
+    it('TS_003Community Group Elder Care', async () => {
+        allureReporter.addDescription(`
+            1. Click on the "Start Now" button.
+            2. Check if the login screen is displayed.
+            3. Enter the username and password.
+            4. Check if the login button is enabled.
+            5. Handle any dialog boxes related to notifications.
+            6. Log in successfully if all steps are completed.
+            7. Click on Elder care
+            8.Ensure that when the user clicks on the right arrow given for the Posts inside the group, the below options are displayed on the screen or not.
+
+        `);
+        try {
+
+            const mail = await readData1("Community", "ElderCare", "Mail", "Testdata1");
+            const Password = await readData1("Community", "ElderCare", "Password", "Testdata1");
+
+            await Keywords.waitForDisplay(locator.startNow, 60000, "Start Now Button")
+            await Keywords.click(locator.startNow, "Start Now Button")
+            const loginDisplay = await Keywords.isDisplayed(locator.loginScreen, "Login screen")
+            expect(loginDisplay).to.be.true;
+
+            await Keywords.SetValue(locator.userName, mail);
+            await Keywords.SetValue(locator.password, Password);
+            if (!await locator.loginButton.isDisplayed()) {
+                await browser.hideKeyboard();
+            }
+            const loginBtnEnable = await Keywords.isEnabled(locator.loginButton, "Login button")
+            expect(loginBtnEnable).to.be.true;
+            await Keywords.click(locator.loginButton, "Login Button")
+            await Keywords.waitForDisplay(locator.allowButton, 20000, "Get notified dialog box")
+            await Keywords.click(locator.allowButton, "Allow button");
+            await Keywords.waitForDisplay(locator.allowNotificationButton, 20000, "Allow notification")
+            await Keywords.click(locator.allowNotificationButton, "Allow notification button");
+            console.log('Login process completed successfully.');
+            await Keywords.waitForDisplay(locator.homePage, 45000, "Home Page")
+            await Keywords.verifyElementDisplayed(community.eldercare, "Elder care")
+            await Keywords.click(community.eldercare, "Elder card");
+            await driver.pause(5000);
+            await Keywords.verifyElementDisplayed(onboardLocator.pickADifferentProgram, "Pick a different program button");
+            await Keywords.verifyElementIsEnabled(onboardLocator.continueButton, "Continue Button");
+            await Keywords.click(onboardLocator.continueButton, "Continue Button");
+            await Keywords.waitForDisplay(community.logo, 45000, "logo");
+            await Keywords.verifyElementDisplayed(community.Nurserecommendations, "Nursere commendations");
+            await Keywords.verifyElementDisplayed(community.CommunityGroups, "Community Groups");
+            await Keywords.verifyElementDisplayed(community.CommunityEvents, "Community Events");
+            await Keywords.verifyElementDisplayed(community.MyResources, "My Resources");
+            await Keywords.verifyElementDisplayed(community.MyResources, "My Resources");
+            await Keywords.verifyElementDisplayed(community.open, "open");
+            await Keywords.click(community.open, "Open Button");
+            await driver.pause(5000);
+            await Keywords.verifyText(community.group, "content-desc", "Test automation group", "Test automation group");
+            await Keywords.verifyElementDisplayed(community.posts, "Posts");
+            await Keywords.click(community.GroupVal, "Test automation group");
+            await Keywords.verifyText(community.GroupValPage, "content-desc", "Test automation group", "Test automation group");
+            await driver.pause(9000);
+            await Keywords.verifyElementDisplayed(community.postesva, "post:");
+            await Keywords.verifyElementDisplayed(community.test, "test");           
+            await Keywords.verifyElementDisplayed(community.newpost, "new post");  
+            await Keywords.verifyElementDisplayed(community.automationdate, "Date");          
+            await Keywords.click(community.testoption, "test option");   
+            await driver.pause(9000);
+            await Keywords.verifyText(community.likepage, "content-desc", "automation post", "automation post");
+            await Keywords.verifyElementDisplayed(community.test, "test");
+            await Keywords.verifyElementDisplayed(community.Browsearticlesabout, "Browse articles about:");
+            await Keywords.verifyElementDisplayed(community.image, "image");    
+            await Keywords.verifyElementIsEnabled(community.addComment, "Add Comment Button");
+            await Keywords.click(community.clickonimage, "Image");   
+            await Keywords.verifyElementDisplayed(community.imagescreen, "Image Screen");    
+            await Keywords.verifyElementDisplayed(community.imagesScreenClose, "Image Screen close");   
+            await Keywords.click(community.imagesScreenClose, "Image Screen close");   
+            await Keywords.verifyElementDisplayed(community.Browsearticlesabout, "Browse articles about:");
+
+        } catch (err) {
+            throw new Error(err);
+        }
+
+    });
+
+
 
 
 
